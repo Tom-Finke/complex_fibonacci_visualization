06689075826ce7b61df4f7a8c8532c05d6f7afc1
Tom Finke <tom.finke@webterra.de>
2021-01-17 14:58:36 +0100

    Added Comments

--- fibo_visualization.py
+++ fibo_visualization.py
@@ -13,23 +13,24 @@ def draw(graphs: list, stepsize: float = 0.001, axes={"x": {"name": "Re(z)"}, "y
 
     fig = plt.figure()
     ax = fig.add_subplot(111)
-    x, y = [], []
-    three_dimensional = False
-    three_dimensional = True if True in [
-        True for graph in graphs if "z_out" in graph] else False
-    print(three_dimensional)
-    if(three_dimensional):
-        z = []
-        ax = fig.gca(projection='3d')
-        ax.set_zlabel(axes[z]["name"]
-                      if "z" in axes and "name" in axes["z"] else "z")
-
+    # Beschriftung der Achsen einfügen und deren Position anpassen
     ax.set_xlabel(
         axes["x"]["name"] if "x" in axes and "name" in axes["x"] else "x", fontsize=15)
     ax.xaxis.set_label_coords(1, 0.45)
     ax.set_ylabel(axes["y"]["name"] if "y" in axes and "name" in axes["y"] else "y",
                   rotation='horizontal', fontsize=15)
     ax.yaxis.set_label_coords(0.5, 1.02)
+
+    # Eruieren ob der Graph 3d oder 2d wird. 3D wenn mind. 1 Graph ein z_out parameter besitzt
+    three_dimensional = True if True in [
+        True for graph in graphs if "z_out" in graph] else False
+
+    if(three_dimensional):
+        ax = fig.gca(projection='3d')
+        ax.set_zlabel(axes[z]["name"]
+                      if "z" in axes and "name" in axes["z"] else "z")
+
+    # Skala Beschriftungen formatieren
     if("x" in axes and "format" in axes["x"]):
         plt.gca().xaxis.set_major_formatter(
             mticker.FormatStrFormatter(axes["x"]["format"]))
@@ -39,67 +40,72 @@ def draw(graphs: list, stepsize: float = 0.001, axes={"x": {"name": "Re(z)"}, "y
     if("z" in axes and "format" in axes["z"] and three_dimensional):
         plt.gca().zaxis.set_major_formatter(
             mticker.FormatStrFormatter(axes["z"]["format"]))
-
+    # Größer der Skala Striche anpassen
     ax.tick_params(axis='x', labelsize=15, pad=10)
     ax.tick_params(axis='y', labelsize=15, pad=20)
-    # Koordinatensystem anpassen --> 2 Achsen die sich in Ursprung treffen
-    ax.spines['left'].set_position('zero')
-    ax.spines['bottom'].set_position('zero')
-    ax.spines['left'].set_linewidth(2)
-    ax.spines['bottom'].set_linewidth(2)
-    ax.spines['right'].set_color('none')
-    ax.spines['top'].set_color('none')
+    ax.tick_params(axis='z', labelsize=15, pad=20) if three_dimensional else 0
 
+    # Wenn 2D Graph, Koordinatenachsen anpassen (Zu Ursprung verschieben) und Pfeile an Koordinatenachsen zeichnen
     if(not three_dimensional):
+        # Koordinatensystem anpassen --> 2 Achsen die sich in Ursprung treffen
+        ax.spines['left'].set_position('zero')
+        ax.spines['bottom'].set_position('zero')
+        ax.spines['left'].set_linewidth(2)
+        ax.spines['bottom'].set_linewidth(2)
+        ax.spines['right'].set_color('none')
+        ax.spines['top'].set_color('none')
         ax.plot((1), (0), ls="", marker=">", ms=10, color="k",
                 transform=ax.get_yaxis_transform(), clip_on=False)
         ax.plot((0), (1), ls="", marker="^", ms=10, color="k",
                 transform=ax.get_xaxis_transform(), clip_on=False)
 
+    # Jeden Graph zeichnen
     for graph in graphs:
         x, y, z = [], [], []
-        for i in range(
+        for i in [x * stepsize for x in range(
                 int(graph["interval"][0] * (1 / stepsize)),
-                int(graph["interval"][1] * (1 / stepsize))):
-            i *= stepsize
+                int(graph["interval"][1] * (1 / stepsize)))]:
             x.append(graph["x_out"](i))
             y.append(graph["y_out"](i))
 
-            z.append(graph["z_out"](
-                i) if "z_out" in graph else 0) if three_dimensional else 0  # Die z-achse wird nur befüllt, wenn einer der Grapghen 3-Dimensional ist
-        print("calculated")
+        # Wenn 3D: z-Achse befüllen und Scatterplot zeichnen
         if(three_dimensional):
+            z.append(graph["z_out"](
+                i) if "z_out" in graph else 0)  # Wenn der Graph keine z-Achse besitzt wird jeder z-wert mit 0 befüllt
             ax.scatter(x, y, z, s=linewidth, zorder=1, color=graph["color"])
         else:
             ax.plot(x, y, linewidth=linewidth, color=graph["color"])
 
+    # Jeden Punkt zeichnen
     for point in points:
-        x = int(point["x"]) if round(point["x"], 1) == int(
-            point["x"]) else point["x"]
-        y = int(point["y"]) if round(point["y"], 1) == int(
-            point["y"]) else point["y"]
-        z = (int(point["z"]) if round(point["z"], 1) == int(
-            point["z"]) else point["z"]) if "z" in point else 0
-        print(z)
+        # Float werte, die auf 10 Nachkommastellen ihren ganzzahlig gerundeten Werten entsprechen werden gerundet, da von einer Abweichung bei der Computer bedingten ungenauigkeit ausgegangen wird
+        for i in ["x", "y", "z"]:
+            point[i] = (int(point[i]) if round(point[i], 10) == int(
+                point[i]) else point[i]) if i in point else 0
+
         if(three_dimensional):
-            ax.scatter(x, y, z, s=linewidth * 2,
+            ax.scatter(point["x"], point["y"], point["z"], s=linewidth * 2,
                        zorder=3, color=point["color"] if "color" in point else "C0")
         else:
-            ax.plot(x, y, color=point["color"] if "color" in point else "C0", marker="o",
+            ax.plot(point["x"], point["y"], color=point["color"] if "color" in point else "C0", marker="o",
                     linewidth=linewidth * 2)
 
         if("annotation" in point and point["annotation"] == True):
+            # Ergebnisse mit mehr als 3 Nachkommastellen werden zur Darstellung gerundet und als z.B. "3.141..."" angegeben
+            x_annotation = point["x"] if round(
+                point["x"], 3) == point["x"] else str(round(point["x"], 3)) + "..."
+            y_annotation = point["y"] if round(
+                point["y"], 3) == point["y"] else str(round(point["y"], 3)) + "..."
 
-            # Ergebnisse mit mehr als 3 Nachkommastellen werden zur Darstellung gerundet
-            annotation = y if round(y, 3) == y else str(round(y, 3)) + "..."
-            ax.annotate(f"({x}|{annotation})",
-                        (x + 0.06, y - 0.5), fontsize=15)
-        if("lines" in point and point["lines"] == True):
+            ax.annotate(f"({x_annotation}|{y_annotation})",
+                        (point["x"], point["y"]), fontsize=15)
 
-            plt.plot([0, x], [y, y],
+        if("lines" in point and point["lines"] == True and not three_dimensional):
+            plt.plot([0, point["x"]], [point["x"], point["y"]],
                      linestyle="dotted", linewidth=2, color=point["color"] if "color" in point else "C0")
-            plt.plot([x, x], [0, y],
+            plt.plot([point["x"], point["x"]], [0, point["y"]],
                      linestyle="dotted", linewidth=2, color=point["color"] if "color" in point else "C0")
+
     plt.show()
 
 
@@ -117,5 +123,5 @@ draw(graphs=[{"interval": [0, 5.7], "x_out": lambda x: binet(x).real, "y_out": l
          "name": "Im(z)", "format": '%.1fi'}, },
      points=[
     {"x": binet(x).real, "y": binet(x).imag, "color": "red",
-     "lines": False, "annotation": False} for x in range(0, 6)
+     "lines": False, "annotation": True} for x in range(0, 6)
 ])
